200ca0ba08ca3dcac3a79725b4b6f5160f6d1ddb
Tonie <victornwanochi@gmail.com>
2023-04-20 10:46:42 +0100

    feat: worked on get and post requests

--- app/main.go
+++ app/main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/", HandleGetVideos)
+
+	http.HandleFunc("/update", HandleUpdateVideos)
 
 	http.ListenAndServe(":8080", nil)
 }
-func Hello(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Hello World!"))
+func HandleGetVideos(w http.ResponseWriter, r *http.Request)  {
+	videos := getVideos()
+
+	videoBytes, err := json.Marshal(videos)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(videoBytes)
+}
+
+func HandleUpdateVideos (w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		var videos []Video
+		err = json.Unmarshal(body, &videos)
+
+		if err != nil {
+			w.WriteHeader(400)
+			fmt.Fprintf(w, "Bad Request")
+		}
+
+		saveVideos(videos)
+
+
+	} else {
+		w.WriteHeader(405)
+		fmt.Fprintf(w, "Method not Supported!")
+	}
+
 }
